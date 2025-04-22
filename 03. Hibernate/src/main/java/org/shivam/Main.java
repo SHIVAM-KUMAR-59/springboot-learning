5d65e2c323314367f8ac1d45f2da85f99e8a3345
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-04-23 00:10:41 +0530

    Saving data in the table

--- 03. Hibernate/src/main/java/org/shivam/Main.java
+++ 03. Hibernate/src/main/java/org/shivam/Main.java
@@ -13,21 +13,30 @@ public class Main {
         s1.setsAge(19);
 
         try{
+
+            /*
             Configuration cfg = new Configuration();
             cfg.addAnnotatedClass(org.shivam.Student.class);
             cfg.configure();
 
-            SessionFactory factory = cfg.buildSessionFactory();
+            // Session factory is a very heavy package
+            // SessionFactory factory = cfg.buildSessionFactory();
+            
+             */
+            SessionFactory factory = new Configuration().addAnnotatedClass(org.shivam.Student.class).configure().buildSessionFactory();
             Session session = factory.openSession();
 
             Transaction transaction = session.beginTransaction();
 
-            session.save(s1);
+            session.persist(s1);
 
             transaction.commit();
 
+            session.clear();
+            factory.close();
+
         }catch(Exception e){
-            e.printStackTrace();
+            System.out.println("Error inserting data!");
         }
 
         System.out.println(s1);
