a142fcd826b31663a8430ec14202f2e898f0231d
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-04-22 23:29:52 +0530

    Getting started with Hibernate

--- 03. Hibernate/src/main/java/org/shivam/Main.java
+++ 03. Hibernate/src/main/java/org/shivam/Main.java
@@ -1,9 +1,25 @@
 package org.shivam;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setsName("Shivam");
+        s1.setRollNo(101);
+        s1.setsAge(19);
 
+        try{
+            Configuration cfg = new Configuration();
+            SessionFactory factory = cfg.buildSessionFactory();
+            Session session = factory.openSession();
+            session.save(s1);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println(s1);
     }
 }
