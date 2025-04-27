b5aa9c71c50c2618a9f014a55c7ef27f61d727f6
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-04-27 15:24:59 +0530

    Model and View

--- 08.SpringBootMVC/src/main/java/com/shivam/_8/SpringBootMVC/HomeController.java
+++ 08.SpringBootMVC/src/main/java/com/shivam/_8/SpringBootMVC/HomeController.java
@@ -5,7 +5,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller // Making a controller (converts this into servlets)
 public class HomeController {
@@ -13,9 +16,12 @@ public class HomeController {
     @RequestMapping("/") // Maps the method to / request
     public String home(){
         System.out.println("Home called");
-        return "index.jsp";
+        // Return the page
+        return "index";
     }
 
+    /*
+    --------------------------- Servlet Way ----------------------
     @RequestMapping("add")
     public String add(HttpServletRequest req, HttpSession session){
 
@@ -25,8 +31,24 @@ public class HomeController {
 
         // Pass the data to the web page
         session.setAttribute("result", result);
-        return "result.jsp";
+        return "result";
 
     }
+     */
 
+    // --------------------------- SpringBoot Way ----------------------
+    @RequestMapping("add")
+    // num1 and num2 should be the same as the ones in the query parameter otherwise use @RequestParam(variable_name)
+    public ModelAndView add(@RequestParam("num1") int num1, @RequestParam("num2") int num2, ModelAndView mv){
+
+        int result = num1 + num2;
+
+        // Pass the data to the web page
+        mv.addObject("result", result);
+
+        // Set the page which needs to be called
+        mv.setViewName("result");
+        return mv;
+
+    }
 }
