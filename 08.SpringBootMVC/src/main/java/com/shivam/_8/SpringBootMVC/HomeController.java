0377d8a170686314148886f9ccf292995e6171d5
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-04-27 14:34:17 +0530

    Sending data

--- 08.SpringBootMVC/src/main/java/com/shivam/_8/SpringBootMVC/HomeController.java
+++ 08.SpringBootMVC/src/main/java/com/shivam/_8/SpringBootMVC/HomeController.java
@@ -1,6 +1,9 @@
 package com.shivam._8.SpringBootMVC;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,19 @@ public class HomeController {
     public String home(){
         System.out.println("Home called");
         return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req, HttpSession session){
+
+        int a = Integer.parseInt(req.getParameter("num1"));
+        int b = Integer.parseInt(req.getParameter("num2"));
+        int result = a + b;
+
+        // Pass the data to the web page
+        session.setAttribute("result", result);
+        return "result.jsp";
+
     }
 
 }
---
+++ 08.SpringBootMVC/src/main/webapp/result.jsp
@@ -0,0 +1,13 @@
+<%@page language="java" %>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    </body>
+        <h2>Calculator</h2>
+
+         <p>Result is: ${result} </p>
+
+    </body>
+</html>
