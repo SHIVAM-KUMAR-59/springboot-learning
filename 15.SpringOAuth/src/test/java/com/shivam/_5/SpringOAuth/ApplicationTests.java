18a4933938458e62933a8c319d2937413e2d2300
SHIVAM-KUMAR-59 <jsrshivamx7@gmail.com>
2025-05-04 15:33:45 +0530

    OAuth2

---
+++ 15.SpringOAuth/src/test/java/com/shivam/_5/SpringOAuth/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam._5.SpringOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
